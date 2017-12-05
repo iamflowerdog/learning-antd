ad1b7714b0325a06409e81c40b660040e3daf6a9
yonghai.yang <yangyonghai@wusongtech.com>
2017-12-05 09:39:22 +0800

    重新构建首页 :apple:

--- index.js
+++ index.js
@@ -9,64 +9,19 @@ import Dynamic from './components/04.DynamicFieldSet';
 import DynamicSelf from './components/05.dynamic-self';
 import D06 from './components/06.dynamic-button';
 import MyTable from './components/07.Table';
+import Entry from './components/08.Entry';
 
 
-function App() {
-    return (
-        <div>
-            <h1>海哥教你玩转antd</h1>
-            <hr/>
-            <br/>
-            <DatePicker/>
-            <br/><br/><br/>
-
-            <hr/>
-
-            <Button type='primary'> primary </Button>
-            <Button> default </Button>
-            <Button type='dashed'> dashed </Button>
-            <Button type='danger'> danger </Button>
-
-            <br/><br/><br/>
-
-            <ButtonSize/>
-
-            <br/><br/><br/>
-
-            <ButtonLoading/>
 
 
-        </div>
-    )
-}
 
-function Root1() {
-    return (
-        <div>
-
-            <D06/>
-        </div>
-    )
-}
-
-function Root2() {
-
-}
 
 ReactDOM.render(
-    <DynamicSelf/>,
-    document.getElementById('root2')
-);
-
-ReactDOM.render(
-    <App/>,
+    <Entry/>,
     document.getElementById("root")
 );
 
-ReactDOM.render(
-    <Root1/>,
-    document.getElementById("root1")
-);
+
 
 
 
